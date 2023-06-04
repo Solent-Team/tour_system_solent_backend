d83873e7c76f101c6842bd4d9b3743093b9bcb65
musthaqmufthi13614 <129644995+musthaqmufthi13614@users.noreply.github.com>
2023-06-04 21:25:39 +0530

    HotelCRUD DELETE & UPDATE

--- src/main/java/com/tour/controller/HotelController.java
+++ src/main/java/com/tour/controller/HotelController.java
@@ -1,5 +1,6 @@
 package com.tour.controller;
 
+import com.tour.exception.UserNotFoundException;
 import com.tour.model.Hotel;
 import com.tour.repository.HotelRepository;
 //import org.springframework.beans.factory.annotation.Autowired;
@@ -35,14 +36,46 @@ public class HotelController {
         return  hotelRepository.findAll();
 }
 
+//to get / load the Specific Dataset using by Unique Attribute value from hotel_table
+    @GetMapping("/hotelget/{hotelid}")
+    Hotel getHotelByID(@PathVariable Integer hotelid){
+
+//        return hotelRepository.findById(hotelid).get();
+        return hotelRepository.findById(hotelid).orElseThrow(()->new UserNotFoundException(hotelid));
+    }
 
 
 
+// to update the required ID Set/ Specific Datasets by using Attribute value from hotel_table
 
+@PutMapping("/hotelget/{hotelid}")
+Hotel updateHotel(@RequestBody Hotel newHotel,@PathVariable Integer hotelid ){
+    return hotelRepository.findById(hotelid)
+            .map(hotel ->{
 
+        hotel.setHotelname(newHotel.getHotelname());
+        hotel.setHoteladdress(newHotel.getHoteladdress());
+        hotel.setHotelemail(newHotel.getHotelemail());
+        hotel.setHotelmobile(newHotel.getHotelmobile());
+        hotel.setRoomcount(newHotel.getRoomcount());
 
+        return hotelRepository.save(hotel);
+            }).orElseThrow(()->new UserNotFoundException(hotelid));
 
+}
 
+//  to delete the required ID Set/ Specific Datasets by using Attribute value from hotel_table
+
+@DeleteMapping("hotelget/{hotelid}")
+String deleteHotel(@PathVariable Integer hotelid){
+        if(!hotelRepository.existsById(hotelid)){
+
+        throw new UserNotFoundException(hotelid);
+
+        }
+        hotelRepository.deleteById(hotelid);
+        return "Hotel with hotelid "+hotelid+" has been deleted succesfully.";
+}
 
 
 
--- src/main/java/com/tour/exception/UserNotFoundException.java
+++ src/main/java/com/tour/exception/UserNotFoundException.java
@@ -4,4 +4,12 @@ public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(Long id) {
         super("user not found" + id);
     }
+
+    public UserNotFoundException(Integer hotelid){
+        super("Could not find the Hotel by the id "+ hotelid);
+    }
+
+
+
+
 }
